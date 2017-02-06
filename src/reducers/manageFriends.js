61a6336ee69d8cbbacc85cadf25a8907b7a7012a
Kevin Wang <kevinwang@Kevins-MacBook-Air.local>
2017-02-06 17:07:05 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,17 @@
 export function manageFriends(state, action){
+
+  if (action.type === "ADD_FRIEND"){
+    return Object.assign({}, state, {friends: [...state.friends, action.payload]})
+  }
+  else if (action.type === "REMOVE_FRIEND"){
+    return Object.assign({}, state, {friends: state.friends.filter(function(friend){
+      if(friend.id !== action.payload){
+        return friend
+      }
+    })})
+  }
+  else {
+    return state
+  }
+
 }
